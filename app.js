af0952b19fe02fe949849b97344b8041cefb07cb
ibkumar <ibkumarandroid@gmail.com>
2024-06-12 18:11:53 +0530

    add highlight while dragging

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ console.log(e.target.id + " is being dragged");
 
 function dragStart(e){
     beingDragged = e.target;
-    console.log("dragging has started on "+ beingDragged);
+    console.log("dragging has started on "+ beingDgitragged);
     infoDisplay.textContent = beingDragged.id + " is beign dragged";
 }
 function dragOver(e){
@@ -32,18 +32,24 @@ function dragOver(e){
     console.log("You are dragging something over "+ e.target.classList)
 }
 function dragEnter(e){
+    e.target.classList.add("highlight");
     console.log("You are entering the spacce of "+ e.target.classList)
 }
 
 function dragLeave(e){
+    e.target.classList.remove("highlight")
     console.log("You are leaving the spacce of "+ e.target.classList)
 }
 
 function dragDrop(e){
     e.target.append(beingDragged)
+    e.target.classList.remove("highlight")
     console.log("You have dropped something into "+ e.target.classList)
 }
 
 function dragEnd(e){
+    e.target.classList.add("target");
+    setTimeout(()=> e.target.classList.remove("target"),100);
     console.log("Your dragged has ended in"+ e.target.classList)
+    infoDisplay.textContent=""
 }
